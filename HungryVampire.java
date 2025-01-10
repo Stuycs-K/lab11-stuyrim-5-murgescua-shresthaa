3504115a10ddf63e5e12da2f400a1a039d4a1668
Adi Murgescu <murgescuadi@gmail.com>
2025-01-10 11:31:56 -0500

    updating code skeleton

--- HungryVampire.java
+++ HungryVampire.java
@@ -14,14 +14,13 @@ public class HungryVampire extends Adventurer{
     }
 
     public CodeWarrior(String name){
-      this(name,24);
+      this(name,30);
     }
 
     public CodeWarrior(){
       this("Morpheus");
     }
 
-    /*The next 8 methods are all required because they are abstract:*/
     public String getSpecialName(){
       return "lifeblood";
     }
@@ -38,28 +37,27 @@ public class HungryVampire extends Adventurer{
       return lifebloodMax;
     }
 
-    /*Deal 2-7 damage to opponent, restores 2 caffeine*/
+    /*Deal 2-4 damage to opponent, restores 2 lifeblood*/
     public String attack(Adventurer other){
-      int damage = (int)(Math.random()*6)+2;
+      int damage = (int)(Math.random()*3)+2;
       other.applyDamage(damage);
       restoreSpecial(2);
       return this + " attacked "+ other + " and dealt "+ damage +
-      " points of damage. They then take a sip of their coffee.";
+      " points of damage. They then take a sip from their jug of lifeblood.";
     }
 
-    /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-    *Reduces caffeine by 8.
+    /*Deal 8 damage to opponent, only if lifeblood is high enough.
+    *Reduces lifeblood by 5; increases hp by 2.
     */
     public String specialAttack(Adventurer other){
       if(getSpecial() >= 8){
-        setSpecial(getSpecial()-8);
-        int damage = (int)(Math.random()*5+Math.random()*5)+3;
+        setSpecial(getSpecial()-5);
+        setHP(getHP()+2);
+        int damage = 8;
         other.applyDamage(damage);
-        return this + " used their "+preferredLanguage+
-        " skills to hack the matrix. "+
-        " This glitched out "+other+" dealing "+ damage +" points of damage.";
+        return this + " used their vampire moves to suck the blood out of"+other+" dealing "+ damage +" points of damage.";
       }else{
-        return "Not enough caffeine to use the ultimate code. Instead "+attack(other);
+        return "Not enough lifeblood to use this ultimate attack. Instead "+attack(other);
       }
 
     }
